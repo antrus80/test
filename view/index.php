f18ed36e3287c810fac613531e351c91035925d5
antrus80 <antrus@ro.ru>
2014-12-09 19:45:22 +0300

    adds1

---
+++ view/index.php
@@ -0,0 +1,14 @@
+<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<html>
+<head>
+    <title>Новости</title>
+</head>
+<body>
+    <?php foreach ($news as $article): ?>
+    <article>
+        <h1><?=$article['title'];?></h1>
+        <div><?=$article['text'];?></div>
+    </article>
+    <?php endforeach; ?>
+</body>
+</html>
